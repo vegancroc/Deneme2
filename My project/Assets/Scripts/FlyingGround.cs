36a3d64776b1afbd4b72a0c24eda08390b51d645
croc <radixwashere@gmail.com>
2022-09-14 19:08:38 +0300

    Uçan Zemin

---
+++ My project/Assets/Scripts/FlyingGround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingGround : MonoBehaviour
+{
+    Transform groundTransform;
+
+    void Start()
+    {
+        groundTransform = this.transform;
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, .01f, 0);
+    }
+}
