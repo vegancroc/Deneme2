dba0cf4d27a8b512b4d7f7ff8d0eae47f0619393
knyeow <ege.yasar37@gmail.com>
2022-09-14 18:36:04 +0300

    Moving Player

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float horizontal;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(horizontal * 5, rb.velocity.y);
+
+    }
+}
